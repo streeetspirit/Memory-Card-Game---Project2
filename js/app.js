a0f7e302da18cbf17034236914ba1d5cdb15d43e
streeetspirit <tracerspirit@gmail.com>
2018-07-16 21:13:25 -0700

    added showing a card face on mouse click

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 /*
- * Create a list that holds all of your cards
+ * List that holds all cards, two for each picture
  */
+let cards = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb","fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
+shuffle(cards);
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+const deck = $(".deck");
+
+for (const card of cards)
+  {
+    let newLi = `<li class="card">
+    <i class="fa ${card}"></i>
+  </li>`
+    deck.append(newLi);
+  }
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +36,43 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * set up the event listener for a deck. If a card is clicked:
+ *  - display the card's symbol
+ *  - add the card to array cardPair of "open" cards
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let cardPair = [];
+
+deck.click(function(event) {
+  const target = $(event.target);
+  if (target.is("li")) {
+    showCard(target);
+    countCard(target);
+  }
+});
+
+function showCard (target) {
+    target.addClass("open");
+}
+
+function countCard(target) {
+  cardPair.push(target);
+  console.log(cardPair.length);
+  if (cardPair.length===2)
+  {
+      if (cardPair[0].className==cardPair[1].className)
+      {
+        console.log(cardPair[0]);
+        console.log(cardPair[1].className);
+        target.addClass("match");
+      }
+      target.removeClass("open");
+      cardPair.splice(0,2);
+  }
+}
